9582a9630340c5db4b0b9a18225d8a063121ada1
hase07180 <yuukisoccer07180@gmail.com>
2020-10-27 09:16:12 +0900

    検索機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_search
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def set_search
+    @search = Tweet.ransack(params[:q])
+    @search_tweets = @search.result
   end
 end
